a50f22e3b90f9491593204b22aee4a442fd7ebca
prernagrg <grgprena2023@gmail.com>
2025-12-10 22:06:47 +0545

    revising bubble sort

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -4,13 +4,11 @@
 void bubbleSort(int arr[], int n)
 {
     int temp;
-    // for n passes
+
     for (int i = 0; i < n - 1; i++)
     {
-        // to avoid sorted elements
         for (int j = 0; j < n - i - 1; j++)
         {
-            // to compare with the adjacent elements and swap
             if (arr[j] > arr[j + 1])
             {
                 temp = arr[j];
@@ -24,13 +22,13 @@ void bubbleSort(int arr[], int n)
 int main()
 {
     int n;
-    printf("Enter No. of elements you want to sort\n");
+
+    printf("Enter no. of elements you want to sort\n");
     scanf("%d", &n);
 
     int *arr = (int *)malloc(n * sizeof(int));
     if (arr == NULL)
     {
-        printf("Memory allocation Failed");
         return 1;
     }
 
@@ -42,13 +40,12 @@ int main()
 
     bubbleSort(arr, n);
 
-    // Display the sorted array
-    printf("Sorted array:\n");
+    printf("Sorted array\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%d\t", arr[i]);
+        printf("%d \t", arr[i]);
     }
-
     free(arr);
+
     return 0;
 }
