d1230fcb8c1bf84c35327440245ae3f3675c811d
prernagrg <grgprena2023@gmail.com>
2025-12-13 21:39:38 +0545

    added insertion_sort

---
+++ sorting/insertion_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, key;
+    int arr[100];
+
+    // Input number of elements
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort Algorithm
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    // Output sorted array
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
